ae019dbdba1c84e58c710e227b39474f6b7f3aed
Restyled.io <commits@restyled.io>
2023-07-08 12:01:18 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -9,15 +9,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://www.propelleraero.com/dirtmate/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  className = "App-link"
+  href = "https://www.propelleraero.com/dirtmate/"
+  target = "_blank"
+  rel = "noopener noreferrer" > Learn React</a>
+      </header><
+        /div>
   );
 }
 
